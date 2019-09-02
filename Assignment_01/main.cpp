848c8e78d4057c37a05f979efc6d62d7b3778a90
bradley <bradley.m.morton@gmail.com>
2019-09-01 20:08:46 -0800

    testing now works. I just need to write tests.

--- Assignment_01/main.cpp
+++ Assignment_01/main.cpp
@@ -5,9 +5,19 @@
 using std::vector;
 #include <algorithm>
 using std::swap;
+using std::is_sorted;
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#define CATCH_CONFIG_FAST_COMPILE
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
+#include "test.cpp"
 
 
-int partition(vector<int> A, int low, int high)
+//This is the algorithm to partition 
+int partition(vector<int>& A, int low, int high)
 {
 	int pivot= A[high];
 	int i = low;
@@ -25,13 +35,13 @@ int partition(vector<int> A, int low, int high)
 }
 
 
-void quicksort(vector<int> A, int low, int high)
+void quickSort(vector<int> & A, int low, int high)
 {
 	if(low < high)
 	{
 		int p = partition(A, low, high);
-		quicksort(A, low, p-1);
-		quicksort(A, p+1, high);
+		quickSort(A, low, p-1);
+		quickSort(A, p+1, high);
 	}
 }
 
@@ -39,8 +49,22 @@ void quicksort(vector<int> A, int low, int high)
 
 
 
-
-int main()
+//Main is where the tests will be run; much of this has been taken from a 311 homework. 
+int main(int argc, char *argv[])
 {
+	Catch::Session session;
+	int catchResult;
+
+	catchResult = session.applyCommandLine(argc, argv);
+
+	if(!catchResult)
+	{
+		cout <<"Beginning quicksort testing" <<endl<<endl;
+		catchResult = session.run();
+		cout<<"Tests ended"<<endl<<endl;
+	}
+
+
+
 	return 0;
 }
