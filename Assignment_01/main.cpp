63908efc5a5a738054002cae9fbd146504d68860
bradley <bradley.m.morton@gmail.com>
2019-09-01 21:56:06 -0800

    I think it works and is good.

--- Assignment_01/main.cpp
+++ Assignment_01/main.cpp
@@ -16,6 +16,8 @@ using std::endl;
 #include "test.cpp"
 
 
+
+
 //This is the algorithm to partition 
 int partition(vector<int>& A, int low, int high)
 {
@@ -37,12 +39,48 @@ int partition(vector<int>& A, int low, int high)
 
 void quickSort(vector<int> & A, int low, int high)
 {
+	if(A.size()==0) return; 
+
 	if(low < high)
 	{
 		int p = partition(A, low, high);
 		quickSort(A, low, p-1);
 		quickSort(A, p+1, high);
 	}
+}
+
+TEST_CASE("Tests")
+{
+    vector<int> data_0{2,6,3,4,9,1};
+    vector<int> data_1;
+    vector<int> data_2{-1,6,-5,11,-72};
+    vector<int> data_3{5,4,3,2,1,0,-1,-2,-3,-4,-5};
+    vector<int> data_4{-5,-4,-3,-2,-1,0,1,2,3,4,5};
+    {
+    	INFO("Positive Integers");
+    	quickSort(data_0, 0, data_0.size()-1);
+    	REQUIRE(is_sorted(data_0.begin(), data_0.end()));
+    }
+    {
+    	INFO("Empty vector");
+    	quickSort(data_1, 0, 0);
+    	REQUIRE(is_sorted(data_1.begin(), data_1.end()));
+    }
+    {
+    	INFO("Positive and negative integers");
+    	quickSort(data_2, 0, data_2.size()-1);
+    	REQUIRE(is_sorted(data_2.begin(), data_2.end()));
+    }
+    {
+    	INFO("Reverse order");
+    	quickSort(data_3, 0, data_3.size()-1);
+    	REQUIRE(is_sorted(data_3.begin(), data_3.end()));
+    }
+    {
+    	INFO("In order");
+    	quickSort(data_4, 0, data_4.size()-1);
+    	REQUIRE(is_sorted(data_4.begin(), data_4.end()));
+    }
 }
 
 
